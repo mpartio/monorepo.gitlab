82aed22ae37516c588602043b4a93c08af2465fc
Mikko Partio <mpartio@gmail.com>
2019-12-13 09:52:07 +0200

    Update last_green_commit.sh
    Show json contents

--- last_green_commit.sh
+++ last_green_commit.sh
@@ -4,6 +4,7 @@ pushd ${DIRNAME}
 header="PRIVATE-TOKEN: ${PRIVATE_TOKEN}"
 url="${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/pipelines?status=success&ref=${CI_COMMIT_REF_NAME}"
 curl --header "${header}"  -s "${url}" -o tmp
+cat tmp
 jq -r -f jq.filter < tmp > .LAST_GREEN_COMMIT
 echo "Last green commit is"
 cat .LAST_GREEN_COMMIT
